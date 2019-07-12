3cd8f7ee6b97c7757f6ef6c7a230458944f03f7e
Vijayaswarnamalya <52819288+Vijayaswarnamalya@users.noreply.github.com>
2019-07-12 20:20:12 +0530

    Update p_seventeen.py

--- p_seventeen.py
+++ p_seventeen.py
@@ -1,4 +1,10 @@
 input_data=input()
 list1=input_data.split(" ")
-result=int(list1[0])*int(list1[1])
-print(result)
+num1=int(list1[0])
+num2=int(list1[1])
+maximum=max(num1,num2)
+while(1):
+    if(maximum%num1==0 and maximum%num2==0):
+        print(maximum)
+        break
+    maximum+=1
