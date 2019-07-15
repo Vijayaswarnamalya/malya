92c141169a0c2b02dbeddb8315fe1da923ebd83c
Vijayaswarnamalya <52819288+Vijayaswarnamalya@users.noreply.github.com>
2019-07-15 15:27:58 +0530

    Update h_3.py

--- h_3.py
+++ h_3.py
@@ -1,7 +1,13 @@
 number=int(input())
 number_input=input()
 list1=list(map(int,number_input.split()))
+l=-1
+flag=0
 for i in range(0,len(list1)):
     if i == list1[i]:
+        flag=1
         print(list1[i],end=' ')
+if flag!=1:
+    print(l)
+        
     
