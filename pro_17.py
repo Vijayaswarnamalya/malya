2721757c674f2e50bfb0511f12870f5d38928b33
Vijayaswarnamalya <52819288+Vijayaswarnamalya@users.noreply.github.com>
2019-07-16 19:49:58 +0530

    Update pro_17.py

--- pro_17.py
+++ pro_17.py
@@ -1,15 +1,12 @@
-alp=input()
-
-a=list(map(int,alp.split()))
-
-k=a[1]
-h=input()
-
-s=list(map(int,h.split()))
+input_num=input()
+list1=list(map(int,input_num.split()))
+k=list1[1]
+num_value=input()
+num_list=list(map(int,num_value.split()))
 flag=0
-for i in range(0,len(s)-1):
-	for j in range(i+1,len(s)):
-		if s[i]+s[j]==k:
+for i in range(0,len(num_list)-1):
+	for j in range(i+1,len(num_list)):
+		if num_list[i]+num_list[j]==k:
 			print("yes")
 			flag=1
 			break
